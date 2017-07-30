379c51e778fa389e781edb05f448106cf629862f
swan <gordienko.c@gmail.com>
2017-07-30 14:49:31 +0300

    Added article component for news output. Stylished output.

--- code/js/app.js
+++ code/js/app.js
@@ -1,4 +1,4 @@
-var my_news = [
+var news = [
     {
         author: 'Саша Печкин',
         text: 'В четверг, четвертого числа...'
@@ -13,6 +13,20 @@ var my_news = [
     }
 ];
 
+var Article = React.createClass({
+    render: function () {
+        var author = this.props.data.author,
+            text = this.props.data.text;
+
+        return (
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className='news__text'>{text}</p>
+            </div>
+        );
+    }
+});
+
 var News = React.createClass({
     render: function() {
         var data = this.props.data;
@@ -22,40 +36,27 @@ var News = React.createClass({
             newsTemplate = data.map(function(item, index) {
                 return (
                     <div key={index}>
-                        <p className="news__author">{item.author}:</p>
-                        <p className="news__text">{item.text}</p>
+                        <Article data={item} />
                     </div>
                 )
             });
         }
 
         return (
-            <div className="news">
+            <div className='news'>
                 {newsTemplate}
-                <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
+                <strong className={'news__count' + (data.length > 0 ? '' : ' none')}>Всего новостей: {data.length}</strong>
             </div>
         );
     }
 });
-
-var Comments = React.createClass({
-    render: function() {
-        return (
-            <div className="comments">
-                Нет новостей - комментировать нечего.
-            </div>
-        );
-    }
-});
-
 
 var App = React.createClass({
     render: function() {
         return (
-            <div className="app">
-                Всем привет, я компонент App! Я умею отображать новости.
-                <News data={my_news} />
-                <Comments />
+            <div className='app'>
+                <h3>Новости</h3>
+                <News data={news} />
             </div>
         );
     }
